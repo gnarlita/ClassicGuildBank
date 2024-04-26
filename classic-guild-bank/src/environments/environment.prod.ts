f0677d71668102a3acc46edc59d0db5977c08afe
Rob Visentin <jvisenti@gmail.com>
2024-04-26 22:17:39 +0200

    Updated frontend

--- classic-guild-bank/src/environments/environment.prod.ts
+++ classic-guild-bank/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: "https://welcome-supplies-api.azurewebsites.net/api",
-  client: "http://welcome.supplies",
+  api: "https://notagb-api.azurewebsites.net/api",
+  client: "https://notagb.com",
   patreon: {
     oauth_redirect: 'https://www.classicguildbank.com/patreon/callback',
     client_id: 'hmjgYD08W4nvMauZip0okmlof5C1bQWr1CfMCJ_pnccqtZobwBbDme4jSvyekSRh'
