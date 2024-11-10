2182dcfd99243a62867737b0cb21cf9d446b90aa
gnarlita <gnarlita@hotmail.com>
2024-11-10 04:52:26 +0000

    fix: replace obsolete EnvironmentName with Environments.Development in Program.cs

--- ClassicGuildBankApi/Program.cs
+++ ClassicGuildBankApi/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using ClassicGuildBankData.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace SSIndustrialApi
@@ -22,7 +20,7 @@ namespace SSIndustrialApi
 
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-            if (environment == EnvironmentName.Development)
+            if (environment == Environments.Development)  // Updated to use Environments.Development
                 SeedDatabase(host);
 
             host.Run();
@@ -34,7 +32,7 @@ namespace SSIndustrialApi
 
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .ConfigureAppConfiguration((hostContext, config) => 
+                .ConfigureAppConfiguration((hostContext, config) =>
                 {
                     config.Sources.Clear();
                     config.AddJsonFile("appsettings.json");
@@ -48,19 +46,20 @@ namespace SSIndustrialApi
 
         private static void SeedDatabase(IWebHost host)
         {
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    try
-            //    {
-            //        var seeder = scope.ServiceProvider.GetService<ClassicGuildBankSeeder>();
-            //        seeder.Seed().Wait();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-            //        logger.LogError(ex, "An error occurred seeding the DB.");
-            //    }
-            //}
+            // Uncomment and modify if database seeding is required
+            // using (var scope = host.Services.CreateScope())
+            // {
+            //     try
+            //     {
+            //         var seeder = scope.ServiceProvider.GetService<ClassicGuildBankSeeder>();
+            //         seeder.Seed().Wait();
+            //     }
+            //     catch (Exception ex)
+            //     {
+            //         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            //         logger.LogError(ex, "An error occurred seeding the DB.");
+            //     }
+            // }
         }
 
         #endregion
